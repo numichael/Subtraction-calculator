1f994bd733babe9f00cf06cfa783e5fc237ae7b0
numichael <numichael.dev@gmail.com>
2021-08-24 23:49:21 +0100

    Initial commit pushing Subtraction Calculator over to github

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 0;
+            int secondNumber = 0;
+            int result = 0;
+
+            Console.Write("Please enter the first number: ");
+            firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the second number:");
+            secondNumber = int.Parse(Console.ReadLine());
+
+            result = firstNumber - secondNumber;
+            Console.WriteLine("The answer is: " + result);
+        }
+    }
+}
